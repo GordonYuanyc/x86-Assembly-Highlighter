427b6f9a24daead65a8b1e6483de19c5d6bd87f6
GordonYuanyc <gordonyuanyc@gmail.com>
2019-10-18 05:34:09 -0500

    ready

---
+++ spec/x86-assembly-highlighter-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import X86AssemblyHighlighterView from '../lib/x86-assembly-highlighter-view';
+
+describe('X86AssemblyHighlighterView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
